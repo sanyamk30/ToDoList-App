356fba93bff769c5496afaa36666cca6bd1f669c
Sanyam <sanyamk30@gmail.com>
2021-07-03 18:21:02 +0530

    Changes to Form, Completed FilterButton Component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,40 @@ import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import { conditionalExpression } from '@babel/types';
 
+const FILTER_MAP = {
+  All: () =>true,
+  Active : task => !task.completed,
+  Completed : task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   
   const [tasks,setTasks] = useState(props.tasks);
+  const [filter,setFilter] = useState('All');
 
-  const taskList = tasks.map(task => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed} 
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask} />
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
   ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter} />
+  ))
 
   function addTask(name){
     const newTask = {id:'todo-' + nanoid(),name:name,completed:false};
@@ -34,6 +55,16 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  function editTask(id,newName){
+    const editedTaskList = tasks.map(task => {
+      if(id == task.id){
+        return {...task,name: newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   function deleteTask(id){
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
@@ -47,9 +78,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
